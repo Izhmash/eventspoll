00242c6201ae9e4b50a1861abf01e768efad8d90
linalin8589 <linalin8589>
2016-12-04 22:47:44 -0500

    lets write a meaningful note:
    i did the create event page. fixed errors
    
    added the get latitude and longitude stuff.
    added event type thing
    still need to figure out time and date

--- app/src/main/java/com/thememeteam/eventspoll/eventspoll.java
+++ app/src/main/java/com/thememeteam/eventspoll/eventspoll.java
@@ -6,23 +6,47 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.FragmentManager;
 import android.app.TimePickerDialog;
+import android.location.Address;
+import android.location.Geocoder;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.DateFormat;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.IOException;
 import java.util.Calendar;
+import java.util.List;
+
+import static android.R.id.list;
 
 public class eventspoll extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eventspoll);
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public void showTimePickerDialog(View v) {
@@ -35,6 +59,41 @@ public class eventspoll extends AppCompatActivity {
         newFragment.show(getFragmentManager(), "datePicker");
     }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("eventspoll Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 
 
     public static class TimePickerFragment extends DialogFragment
@@ -51,6 +110,7 @@ public class eventspoll extends AppCompatActivity {
             return new TimePickerDialog(getActivity(), this, hour, minute,
                     DateFormat.is24HourFormat(getActivity()));
         }
+
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
 
         }
@@ -75,6 +135,28 @@ public class eventspoll extends AppCompatActivity {
         public void onDateSet(DatePicker view, int year, int month, int day) {
             //
         }
+    }
+
+    public void geoLocate(View v) throws IOException {
+        hideSoftKeyboard(v);
+
+        EditText et = (EditText) findViewById(R.id.editText2);
+        String location = et.getText().toString();
+
+        Geocoder gc = new Geocoder(this);
+        List<Address> addresses;
+        addresses = gc.getFromLocationName(location, 1);
+        if(addresses.size() > 0) {
+            double latitude= addresses.get(0).getLatitude();
+            double longitude= addresses.get(0).getLongitude();
+            Toast.makeText(this,Double.toString(latitude) , Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,Double.toString(longitude) , Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void hideSoftKeyboard(View v) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
     }
 
 }
