476d8847a42b95756a84ec5ce0ff756cbdaeee55
linalin8589 <linalin8589>
2016-12-04 19:52:38 -0500

    lets write a meaningful note:
    i did the create event page. fixed errors

--- app/src/androidTest/java/com/thememeteam/eventspoll/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/thememeteam/eventspoll/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.thememeteam.eventspoll", appContext.getPackageName());
+        assertEquals("com.thememeteam.activity_eventspoll", appContext.getPackageName());
     }
 }
